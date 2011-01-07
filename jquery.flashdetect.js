2e5bf163a941696aca0aeb1d808122baef64323d
Torsten Curdt <tcurdt@vafer.org>
2011-01-07 13:28:50 +0100

    cleanup

--- jquery.flashdetect.js
+++ jquery.flashdetect.js
@@ -1,11 +1,11 @@
 /**
  * jquery.flashdetect plugin
- * 
+ *
  * based on http://jquery.lukelutman.com/plugins/flash
  **/
 
 (function($){
-	
+
 /**
  * @name flash.hasFlash
  * @desc Check if a specific version of the Flash plugin is installed
@@ -13,21 +13,21 @@
  *
 **/
 $.hasFlash = function(arguments) {
-    arguments = typeof(arguments) != 'undefined' ? arguments : [1,0,0];    
-	// look for a flag in the query string to bypass flash detection
-	if(/hasFlash=true/.test(location)) return true;
-	if(/hasFlash=false/.test(location)) return false;
-	var pv = $.hasFlash.playerVersion().match(/\d+/g);
-	for(var i = 0; i < 3; i++) {
-		var p = parseInt(pv[i] || 0);
-		var r = arguments[i] || 0;
-		// player is less than required
-		if(p < r) return false;
-		// player is greater than required
-		if(p > r) return true;
-	}
-	// major version, minor version and revision match exactly
-	return true;
+    arguments = typeof(arguments) != 'undefined' ? arguments : [1,0,0];
+  // look for a flag in the query string to bypass flash detection
+  if(/hasFlash=true/.test(location)) return true;
+  if(/hasFlash=false/.test(location)) return false;
+  var pv = $.hasFlash.playerVersion().match(/\d+/g);
+  for(var i = 0; i < 3; i++) {
+    var p = parseInt(pv[i] || 0);
+    var r = arguments[i] || 0;
+    // player is less than required
+    if(p < r) return false;
+    // player is greater than required
+    if(p > r) return true;
+  }
+  // major version, minor version and revision match exactly
+  return true;
 };
 
 /**
@@ -36,25 +36,25 @@ $.hasFlash = function(arguments) {
  * @type String
 **/
 $.hasFlash.playerVersion = function() {
-	// ie
-	try {
-		try {
-			// avoid fp6 minor version lookup issues
-			// see: http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
-			var axo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash.6');
-			try { axo.AllowScriptAccess = 'always';	} 
-			catch(e) { return '6,0,0'; }				
-		} catch(e) {}
-		return new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version').replace(/\D+/g, ',').match(/^,?(.+),?$/)[1];
-	// other browsers
-	} catch(e) {
-		try {
-			if(navigator.mimeTypes["application/x-shockwave-flash"].enabledPlugin){
-				return (navigator.plugins["Shockwave Flash 2.0"] || navigator.plugins["Shockwave Flash"]).description.replace(/\D+/g, ",").match(/^,?(.+),?$/)[1];
-			}
-		} catch(e) {}		
-	}
-	return '0,0,0';
+  // ie
+  try {
+    try {
+      // avoid fp6 minor version lookup issues
+      // see: http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
+      var axo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash.6');
+      try { axo.AllowScriptAccess = 'always'; }
+      catch(e) { return '6,0,0'; }
+    } catch(e) {}
+    return new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version').replace(/\D+/g, ',').match(/^,?(.+),?$/)[1];
+  // other browsers
+  } catch(e) {
+    try {
+      if(navigator.mimeTypes["application/x-shockwave-flash"].enabledPlugin){
+        return (navigator.plugins["Shockwave Flash 2.0"] || navigator.plugins["Shockwave Flash"]).description.replace(/\D+/g, ",").match(/^,?(.+),?$/)[1];
+      }
+    } catch(e) {}
+  }
+  return '0,0,0';
 };
-	
+
 })(jQuery);
--- jquery.fullslide.js
+++ jquery.fullslide.js
@@ -3,9 +3,9 @@
  **/
 
 (function($){
-    
+
     var
-    
+
     options,
     previous_slide,
     index = 0,
@@ -13,10 +13,10 @@
     front,
     back,
     timer;
-    
+
     // API: main init function
     $.fullslide = function(in_options) {
-    
+
         // default options
         options = $.extend({
             fade_duration: 750,
@@ -30,7 +30,7 @@
         arguments.callee.pause = function() {
             if (!pause) {
                 pause = true;
-                clearTimeout(timer);                
+                clearTimeout(timer);
             }
         };
 
@@ -60,67 +60,67 @@
             var
             link,
             img;
-            
+
             for(var i=0; i<options.slides.length; i++) {
                 link = $("<a />")
                     .click(function(idx) {
                         return function() { jQuery.fullslide.show(idx) };
                     }(i));
-                
+
                 img = $("<img />")
                     .attr('src', options.slides[i]['thumb'])
                     .attr('alt', options.slides[i]['title'])
                     .addClass( (i==index)?'selected':'unselected')
                     .appendTo(link);
-                
+
                 options.slides[i]['thumb img'] = img;
-                
-                link.appendTo(wrap);                
+
+                link.appendTo(wrap);
           }
         };
 
         $(document).ready(_init);
-    
+
         return this;
 
-    
+
         function _init() {
 
             var
-    
+
             common = { left: 0, top: 0 },
-    
+
             wrap = $("<div />")
                 .attr("id", "fullslide-wrap")
                 .css( $.extend(common, { position: "absolute", zIndex: -1 }));
-    
+
             // if enable use a blank.gif overlay to protect the images
             if (options.blank_overlay) {
 
                 var
-    
+
                 blank = $("<div />")
                   .attr("id", "fullslide-blank")
                   .css( $.extend(common, { position: "fixed", overflow: "hidden", zIndex: 1, width: 4000, height: 2000 }))
                   .appendTo(wrap),
-        
+
                 img_blank = $("<img />")
                   .attr("src", "images/blank.gif")
                   .css("width", "100%")
                   .css("height", "100%")
                   .appendTo(blank);
             }
-    
+
             // the actual buffers. we load in the back, then make it translucent.
             // switch the backbuffer to front and fade it in.
 
             var
-        
+
             buffer1 = $("<div />")
                 .attr('id', 'fullslide-1')
                 .css( $.extend(common, { position: "fixed", overflow: "hidden", zIndex: -2 }))
                 .appendTo(wrap),
-    
+
             buffer2 = $("<div />")
                 .attr('id', 'fullslide-2')
                 .css( $.extend(common, { position: "fixed", overflow: "hidden", zIndex: -3 }))
@@ -130,18 +130,18 @@
                 .bind("load", function(event) {
                     _load(event, $(this));
                 }).appendTo(buffer1),
-    
+
             img2 = $("<img />")
                 .bind("load", function(event) {
                     _load(event, $(this));
                 }).appendTo(buffer2);
-    
+
             // generate an index property so we can find
             // the index from a slide array
             for (var i=0; i<options.slides.length; i++) {
                 options.slides[i]['index'] = i;
             }
-    
+
             $('body').prepend(wrap);
 
             // show only "loading" at the beginning
@@ -154,7 +154,7 @@
 
             // load the first image
             _load_slide_from_index();
-            
+
             // resize and position the images on window resize
             $(window).resize(_resize_window);
         }
@@ -166,7 +166,7 @@
             tmp = back;
             back = front;
             front = tmp;
-            
+
             front.css('zIndex', -2);
             back.css('zIndex', -3);
         }
@@ -177,7 +177,7 @@
             // remember original image ratio and resize
             image.data('ratio', image.width() / image.height());
             _resize_image(image);
-            
+
             // make sure "loading" is hidden
             $('#loading').hide();
             $('#content').show();
@@ -185,7 +185,7 @@
             // callback for slide changes
             options.on_slide_callback(previous_slide, options.slides[index]);
             previous_slide = options.slides[index];
-            
+
             // do the double-buffering and fading dance
             front.css('opacity', 1);
             back.css('opacity', 0);
@@ -208,41 +208,41 @@
                     }
 
                     _load_slide_from_index();
-                    
+
                 }, options.show_duration);
             });
-            
+
         }
 
         // loads the image into the background buffer
         function _load_slide_from_index() {
             back.find('img').attr('src', '').css('width', 'auto').css('height', 'auto').attr('src', options.slides[index]['src']);
         }
-    
+
         // adjust and position the images. called on window resize.
         function _resize_window() {
             _resize_image(front.find('img'));
             _resize_image(back.find('img'));
         }
-    
+
         // adjust and position a single image
         function _resize_image(image, on_resize_callback) {
             var
-    
+
             browserwidth = $(window).width(),
             browserheight = $(window).height(),
             bgWidth = browserwidth,
             bgHeight = bgWidth / image.data('ratio');
-            
+
             if(bgHeight < browserheight) {
                 bgHeight = browserheight;
                 bgWidth = bgHeight * image.data('ratio');
             }
-    
+
             image.width( bgWidth ).height( bgHeight );
             image.parent().css('left', (browserwidth - bgWidth)/2);
             image.parent().css('top', (browserheight - bgHeight)/2);
-    
+
             if (typeof on_resize_callback == "function") on_resize_callback();
         }
     };
